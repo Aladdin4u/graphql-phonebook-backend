42fa47619df9d4656e5d986ccbaf1c9905a82b3a
aladdin4u <taofikakinkumi@gmail.com>
2023-06-11 17:34:26 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import { gql, useQuery } from '@apollo/client'
-import Persons from './components/Persons'
-import PersonForm from './components/PersonForm'
+import { gql, useQuery } from "@apollo/client";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import { ALL_PERSONS } from "./queries";
+import PhoneForm from "./components/PhoneForm";
 
-const ALL_PERSONS = gql`
-query {
-  allPersons {
-    name
-    phone
-    id
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
   }
-}
-`
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
+};
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const result = useQuery(ALL_PERSONS, {
-    pollInterval: 2000
-  })
+    pollInterval: 2000,
+  });
 
   if (result.loading) {
-    return <div>loading...</div>
+    return <div>loading...</div>;
   }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
 
   return (
     <div>
-     <Persons persons={result.data.allPersons}/>
-     <PersonForm />
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import { gql, useMutation } from '@apollo/client'
+import { useMutation, onError } from '@apollo/client'
+import { CREATE_PERSON } from '../queries'
 
-const CREATE_PERSON = gql`
-mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String) {
-  addPerson(
-    name: $name,
-    street: $street,
-    city: $city,
-    phone: $phone
-  ) {
-    name
-    phone
-    id
-    address {
-      street
-      city
-    }
-  }
-}
-`
-
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
   const [street, setStreet] = useState('')
   const [city, setCity] = useState('')
 
 
-  const [ createPerson ] = useMutation(CREATE_PERSON)
+  const [ createPerson ] = useMutation(CREATE_PERSON, {
+    refetchQueries: [ { query: ALL_PERSONS } ],
+    onError: (error) => {
+      const messages = error.graphQLErrors[0].message
+      setError(messages)
+    }
+  })
 
   const submit = (event) => {
     event.preventDefault()
